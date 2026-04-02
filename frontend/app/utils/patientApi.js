06362942125c5c232f6bd194c24eff711e9c8c3e
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-04-02 21:24:53 +0300

    feat(patient): add patientApi utility functions

---
+++ frontend/app/utils/patientApi.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000';
+
+const authHeaders = () => ({
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+});
+
+export const getPatientDashboard = async () => {
+  const res = await axios.get(`${BASE_URL}/patient-portal/dashboard`, authHeaders());
+  return res.data;
+};
+
+export const getPatientProfile = async () => {
+  const res = await axios.get(`${BASE_URL}/patient-portal/profile`, authHeaders());
+  return res.data;
+};
+
+export const updatePatientProfile = async (data) => {
+  const res = await axios.put(`${BASE_URL}/patient-portal/profile`, data, authHeaders());
+  return res.data;
+};
+
+export const getPatientAppointments = async () => {
+  const res = await axios.get(`${BASE_URL}/patient-portal/appointments`, authHeaders());
+  return res.data;
+};
+
+export const bookAppointment = async (data) => {
+  const res = await axios.post(`${BASE_URL}/patient-portal/appointments`, data, authHeaders());
+  return res.data;
+};
+
+export const cancelAppointment = async (id) => {
+  const res = await axios.delete(`${BASE_URL}/patient-portal/appointments/${id}`, authHeaders());
+  return res.data;
+};
+
+export const getPatientMedicalRecords = async () => {
+  const res = await axios.get(`${BASE_URL}/patient-portal/medical-records`, authHeaders());
+  return res.data;
+};

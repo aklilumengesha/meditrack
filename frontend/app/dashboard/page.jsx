67753ea82386309a63b50037cbc4b8a4f6bce9d1
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-04-02 20:27:28 +0300

    feat: add dashboard page

---
+++ frontend/app/dashboard/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import Header from '../components/Header';
+import Dashboard from '../components/dashboard/Dashboard';
+
+const page = () => {
+  return (
+    <>
+        <Header />
+        <Dashboard/>
+    </>
+  )
+}
+
+export default page

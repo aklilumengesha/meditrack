63c1719cc51152950848f3e1a6d590b965d7c32d
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-04-03 12:10:46 +0300

    feat(admin): expand adminApi with all CRUD utility functions

--- frontend/app/utils/adminApi.js
+++ frontend/app/utils/adminApi.js
@@ -1,42 +1,39 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3000';
+const authHeaders = () => ({ headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
 
-const authHeaders = () => ({
-  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-});
+// Stats
+export const getAdminStats = async () => (await axios.get(`${BASE_URL}/admin/stats`, authHeaders())).data;
+export const getMonthlyStats = async () => (await axios.get(`${BASE_URL}/admin/stats/monthly`, authHeaders())).data;
 
-export const getAdminStats = async () => {
-  const res = await axios.get(`${BASE_URL}/admin/stats`, authHeaders());
-  return res.data;
-};
+// Users
+export const getAllUsers = async () => (await axios.get(`${BASE_URL}/admin/users`, authHeaders())).data;
+export const deleteUser = async (id) => (await axios.delete(`${BASE_URL}/admin/users/${id}`, authHeaders())).data;
+export const toggleUserActive = async (id) => (await axios.patch(`${BASE_URL}/admin/users/${id}/toggle-active`, {}, authHeaders())).data;
+export const changeUserRole = async (id, role) => (await axios.patch(`${BASE_URL}/admin/users/${id}/role`, { role }, authHeaders())).data;
+export const resetUserPassword = async (id, password) => (await axios.patch(`${BASE_URL}/admin/users/${id}/reset-password`, { password }, authHeaders())).data;
 
-export const getAllUsers = async () => {
-  const res = await axios.get(`${BASE_URL}/admin/users`, authHeaders());
-  return res.data;
-};
+// Doctors
+export const getAdminDoctors = async () => (await axios.get(`${BASE_URL}/admin/doctors`, authHeaders())).data;
+export const createAdminDoctor = async (data) => (await axios.post(`${BASE_URL}/admin/doctors`, data, authHeaders())).data;
+export const updateAdminDoctor = async (id, data) => (await axios.put(`${BASE_URL}/admin/doctors/${id}`, data, authHeaders())).data;
+export const getDoctorAppointmentsAdmin = async (id) => (await axios.get(`${BASE_URL}/admin/doctors/${id}/appointments`, authHeaders())).data;
 
-export const deleteUser = async (id) => {
-  const res = await axios.delete(`${BASE_URL}/admin/users/${id}`, authHeaders());
-  return res.data;
-};
+// Patients
+export const getAdminPatients = async () => (await axios.get(`${BASE_URL}/admin/patients`, authHeaders())).data;
+export const updateAdminPatient = async (id, data) => (await axios.put(`${BASE_URL}/admin/patients/${id}`, data, authHeaders())).data;
+export const getPatientRecordsAdmin = async (id) => (await axios.get(`${BASE_URL}/admin/patients/${id}/records`, authHeaders())).data;
 
-export const getAdminDoctors = async () => {
-  const res = await axios.get(`${BASE_URL}/admin/doctors`, authHeaders());
-  return res.data;
-};
+// Appointments
+export const getAdminAppointments = async () => (await axios.get(`${BASE_URL}/admin/appointments`, authHeaders())).data;
+export const createAdminAppointment = async (data) => (await axios.post(`${BASE_URL}/admin/appointments`, data, authHeaders())).data;
+export const updateAdminAppointment = async (id, data) => (await axios.put(`${BASE_URL}/admin/appointments/${id}`, data, authHeaders())).data;
+export const deleteAdminAppointment = async (id) => (await axios.delete(`${BASE_URL}/admin/appointments/${id}`, authHeaders())).data;
 
-export const getAdminPatients = async () => {
-  const res = await axios.get(`${BASE_URL}/admin/patients`, authHeaders());
-  return res.data;
-};
+// Medical Records
+export const getAllMedicalRecords = async () => (await axios.get(`${BASE_URL}/admin/medical-records`, authHeaders())).data;
+export const deleteAdminMedicalRecord = async (id) => (await axios.delete(`${BASE_URL}/admin/medical-records/${id}`, authHeaders())).data;
 
-export const getAdminAppointments = async () => {
-  const res = await axios.get(`${BASE_URL}/admin/appointments`, authHeaders());
-  return res.data;
-};
-
-export const deleteAdminAppointment = async (id) => {
-  const res = await axios.delete(`${BASE_URL}/admin/appointments/${id}`, authHeaders());
-  return res.data;
-};
+// Settings
+export const getSpecialties = async () => (await axios.get(`${BASE_URL}/admin/specialties`, authHeaders())).data;
